60ccf7c998d26e46a7d2ae5a69db93c96259096e
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-06-18 11:39:25 +0200

    Mitarbeite_databank

---
+++ mitarbeite_data/hello_rust/target/debug/deps/hello_rust-e4b6d5ed0b290c0c.d
@@ -0,0 +1,5 @@
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/mitarbeite_data/hello_rust/target/debug/deps/hello_rust-e4b6d5ed0b290c0c.d: src/main.rs
+
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/mitarbeite_data/hello_rust/target/debug/deps/libhello_rust-e4b6d5ed0b290c0c.rmeta: src/main.rs
+
+src/main.rs:
