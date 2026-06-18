6bb63a64ea33d970833299ae699c1e4c41d9cdb8
Salih Kayan <salihkayan165@gmail.com>
2026-06-18 14:38:23 +0200

    Knightrider Übung

---
+++ Knightrider/Knightrider.ino
@@ -0,0 +1,26 @@
+int pinArray[] = {12, 11, 10, 9, 8, 7};
+int count = 0;
+int timer = 50;
+ 
+void setup(){
+  for (count=0;count<6;count++) {
+    pinMode(pinArray[count], OUTPUT);
+  }
+}
+ 
+void loop() {
+  for (count=0;count<5;count++) { 
+      digitalWrite(pinArray[count], HIGH); 
+      delay(timer); digitalWrite(pinArray[count +1], HIGH); 
+      delay(timer); digitalWrite(pinArray[count], LOW); 
+      delay(timer*2); } 
+      
+      for (count=5;count>0;count--) {
+      digitalWrite(pinArray[count], HIGH);
+      delay(timer);
+      digitalWrite(pinArray[count - 1], HIGH);
+      delay(timer);
+      digitalWrite(pinArray[count], LOW);
+      delay(timer*2);
+    }
+}
